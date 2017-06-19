6e67890b1078587a7084f3a919dd2aea7d2a4dcf
KarinaLaroze <karinalaroze@gmail.com>
2017-06-19 00:06:49 -0400

    trabajando en js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,14 @@
 /*aquí va tu código*/
+function clickBoton(){
+	var boton = document.getElementById("boton-guardar");
+	clickBoton.addEventListener("click", guardarDatos);
+	guardarDatos();
+     
+}
+
+function guardarDatos(){
+	localStorage.clave = document.getElementById("clave").value;
+	localStorage.valor = document.getElementById("valor").value;
+
+}
+
